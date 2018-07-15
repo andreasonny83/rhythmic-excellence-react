15190527949cf473250e58a0a28ab42cb05fd933
Andrea Sonny <andreasonny83@gmail.com>
2018-07-15 13:58:52 +0100

    build(TravisCI): TravisCI for CD with NOW

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rhythmic Excellence (React)
 
 > Rhythmic Excellence rebuilt with React
+
+[![Build Status](https://travis-ci.org/andreasonny83/rhythmic-excellence-react.svg?branch=master)](https://travis-ci.org/andreasonny83/rhythmic-excellence-react)
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = 'http://rhythmicexcellence.london/wp-json/wp/v2/';
-export const ASSETS_ENDPOINT = 'http://rhythmicexcellence.london/';
+export const API_ENDPOINT = 'https://rhythmicexcellence.london/wp-json/wp/v2/';
+export const ASSETS_ENDPOINT = 'https://rhythmicexcellence.london/';
